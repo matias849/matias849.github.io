c5d9d115c1df91334dd6edc81971b3e1136fdadb
matias849 <149196920+matias849@users.noreply.github.com>
2024-11-05 14:32:32 +0000

    cambio

---
+++ proyecto-git/apuestas-en-linea/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+      alert("Atención: las apuestas pueden afectar tu rendimiento y salud mental. Consulta a un adulto de confianza si tienes dudas.");
+      });
+})
